1b04027023fcca8e8c7eef0756e6b603baeab313
akashchandrashrivastava <30461547+akashchandrashrivastava@users.noreply.github.com>
2017-07-26 16:54:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/emergency/MyHealth2Activity.java
@@ -0,0 +1,13 @@
+package com.example.android.emergency;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyHealth2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_health2);
+    }
+}
